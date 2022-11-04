e7ec192576b0b85ff57750d132a2208d5f59beb1
ilham-asgarli <ilhamasgarli19@gmail.com>
2022-11-04 11:05:33 +0300

    Initial commit

--- mongo.js
+++ mongo.js
@@ -1,9 +1,15 @@
 var mongoose = require("mongoose")
 
 var MONGODB_URI = process.env.MONGODB_URI
+const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[/(:\\/@/]+'));
 
 module.exports = async () => {
-    await mongoose.connect(MONGODB_URI, {useMongoClient: true}).then((result) => {
+    await mongoose.connect(MONGODB_URI, {
+        useMongoClient: true, proxyUsername: fixieData[0],
+        proxyPassword: fixieData[1],
+        proxyHost: fixieData[2],
+        proxyPort: fixieData[3]
+    }).then((result) => {
         console.log("Mongo connected...")
     }).catch((err) => {
         console.log(err)
