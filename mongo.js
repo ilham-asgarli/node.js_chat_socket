2fd28822b1da6df581709b8f053f308736d09abe
ilham-asgarli <ilhamasgarli19@gmail.com>
2022-11-04 09:38:07 +0300

    Initial commit

--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose")
 var MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = () => {
-    mongoose.connect(MONGODB_URI);
+    mongoose.connect(MONGODB_URI, {useMongoClient: true});
 
     mongoose.connection.on('open', () => {
         console.log('MongoDB: Connected');
